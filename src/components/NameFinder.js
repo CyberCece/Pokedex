a9b76a1399bed999cc291123f7927166578cbefe
Cecilia López Maxías <cece@MacBook-Pro.local>
2023-04-07 17:29:57 +0200

    minor adjustments

--- src/components/NameFinder.js
+++ src/components/NameFinder.js
@@ -4,13 +4,14 @@ export default function NameFinder({ currentName, setCurrentName }) {
   return(
     <div>
       <input
-      className='flex pt-4 capitalize w-60 placeholder-white text-4xl bg-red-500'
+      className='flex pt-4 text-center capitalize w-52 placeholder-white text-4xl bg-red-500'
       type ="text"
       placeholder={currentName}
       onKeyDown={ (event) => {
         if (event.key === 'Enter') {
           setCurrentName(() => {
-            return event.target.value.toLowerCase()
+            const value = event.target.value || "default"
+            return value.toLowerCase()
           })
         } 
       }}
